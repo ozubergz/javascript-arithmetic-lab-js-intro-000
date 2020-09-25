014627e3dd7416688048a69bb8b6b16cf68fcccd
James Min <jmin499@gmail.com>
2020-09-25 19:40:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,27 +1,23 @@
-function add() {
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b
+}
+
+function multiply(a, b) {
+  return a * b
+}
+
+function divide(a, b) {
+  return a/b
+}
+
+function increment(a) {
 
 }
 
-function add() {
-
-}
-
-function subtract() {
-
-}
-
-function multiply() {
-
-}
-
-function divide() {
-
-}
-
-function add() {
-
-}
-
-function add() {
+function decrement() {
 
 }
