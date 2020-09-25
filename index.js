4c24b875cd7a8166c56cc45d5a9bdeb97725978d
James Min <jmin499@gmail.com>
2020-09-25 19:40:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,10 +14,10 @@ function divide(a, b) {
   return a/b
 }
 
-function increment(a) {
-
+function increment(n) {
+  return n++
 }
 
-function decrement() {
-
+function decrement(n) {
+  return n--
 }
